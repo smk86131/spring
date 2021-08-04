8ee62840c4c2a5ac6173e8d45e63dad33780e333
smk86131 <81944293+smk86131@users.noreply.github.com>
2021-08-04 16:29:08 +0900

    Add files via upload

--- org/src/main/java/org/MainConfig.java
+++ org/src/main/java/org/MainConfig.java
@@ -1,9 +1,21 @@
 package org;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractRefreshableConfigApplicationContext;
+
 public class MainConfig {
 	
 	public static void main(String[] args) {
+		AnnotationConfigApplicationContext acac =
+				new AnnotationConfigApplicationContext(SpringConfig.class);
 		
+		AA a1 = acac.getBean(AA.class);
+		AA a2 = acac.getBean(AA.class);
+		
+		System.out.println(a1);
+		System.out.println(a2);
+		
+		acac.close();
 	}
 
 }
